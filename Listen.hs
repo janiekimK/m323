1562c182ef5d7ed3edb53b3728baa92599b75f6d
janiekimk <janie.kabashi@bbzbl-it.ch>
2024-08-27 13:55:25 +0200

    New Solutions

---
+++ Listen.hs
@@ -0,0 +1,38 @@
+-- Aufgaben
+-- Erstellen Sie eine Liste mit den Zahlen von 1 bis 10.
+
+
+
+-- Erstellen Sie eine Liste mit den Kubikzahlen der Zahlen von 1 bis 10.
+
+-- Geben Sie die Summe der Werte in der Liste aus Aufgabe 2 aus.
+
+-- Schreiben Sie eine Funktion,die zwei ganze Zahlen erhält und eine Liste zurückgibt, mit allen Zahlen im Bereich der beiden Zahlen.
+
+-- Schreiben Sie eine Funktion, die eine Zahl als Parameter n erhält. Die Funktion soll dann die Summe der ersten n Werte 1/x zurückgeben
+--  (also 1/1 + 1/2 + 1/3 + 1/4 + ... + 1/n).
+
+-- Schreiben Sie eine Funktion, die Ihnen das zweitletzte Element einer Liste zurückgibt.
+
+-- Schreiben Sie eine rekursive Funktion, welche die Anzahl Leerzeichen in einem String zählt. 
+-- (Beachten Sie dazu den Abschnitt "Pattern matching" im obigen Video.)
+
+-- Ein Palindrom ist z.B. ein Wort, das vorwärts und rückwärts gelesen gleich ist. Schreiben Sie eine Funktion, die herausfindet, 
+-- ob eine Liste ein Palindrom ist (ob sie also in beide Richtungen gelesen gleich ist)
+-- Wenn Sie mit einer Typvariabeln arbeiten, also mit einer Liste vom Typ [a], dann muss der Datentyp die Einschränkung haben, 
+-- dass Werte verglichen werden können. Deshalb muss die Funktion 
+-- z.B. die folgende 1. Zeile haben: palindrome :: Eq a => [a] -> Bool (Dabei ist Eq a eben die erwähnte Einschränkung.)
+-- Es gibt eine Funktion reverse, die helfen kann.
+-- Versuchen Sie aber auch eine rekursive Lösung zu finden.
+
+-- Schreiben Sie eine Funktion dupli, die eine Liste als Parameter erhält und eine Liste zurückgibt, in der jedes Element doppelt vorkommt. 
+-- Aus der Liste [4,2,8,3] soll die Liste [4,4,2,2,8,8,3,3] erstellt werden.
+
+-- Informieren Sie sich über die Haskell-Funktion words. Schreiben Sie dann eine Funktion, die einen String erhält ([Char]).
+-- Dieser String soll einen Satz enthalten. Geben Sie das erste Wort des Satzes zurück.
+
+-- Gehen Sie auf die Seite 99 Haskell Probleme und versuchen Sie die folgenden Aufgaben zu lösen: 1, 3, 4, 14. 
+-- Diese Aufgaben sind mit Befehlen lösbar, die wir bereits kennen. Versuchen Sie aber auch eigene Lösungen (mit Rekursion) zu finden.
+-- Es gibt dort zu jeder Aufgabe Musterlösungen. Lassen Sie sich nicht erschlagen von der Vielzahl der Musterlösungen. 
+-- Bei den genannten Aufgaben hat es immer mindestens eine Lösung dabei, die für uns bereits nachvollziehbar ist. 
+-- Von den anderen Lösungen kann man viel lernen. Sie können auch andere Aufgaben lösen, die angegebenen passen einfach gerade zu unserem Wissensstand.
