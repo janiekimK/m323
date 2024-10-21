fed43256eef4e37586b456fb05ba3bc249f02fc3
Janie Kabashi <137904239+janiekimK@users.noreply.github.com>
2024-10-21 11:07:00 +0200

    Update Rekursion.hs
    Eigene Lösungen zu Aufgaben eingefügt

--- Rekursion.hs
+++ Rekursion.hs
@@ -1,41 +1,14 @@
--- 1
-summe :: Integer -> Integer
-summe 0 = 0
-summe n = n + summe (n - 1)
+--Aufgbae 1
+--Schreiben Sie eine Funktion, die eine ganze Zahl als Parameter erhält. Die Funktion soll die Summe aller Zahlen von 1 bis zu dieser Zahl zurückgeben.
 
--- 2
-fact :: Integer -> Integer
-fact 0 = 1
-fact n = n * fact (n - 1)
+sumNum :: Int -> Int
+sumNum 0 = 0
+sumNum n =  n + sumNum ( n - 1) 
 
--- 3
+--Aufgbae 2
+--Schreiben Sie eine Funktion, die eine Integer Zahl als Parameter erhält. Die Funktion soll die Fakultät dieser Zahl zurückgeben. (Fakultät von x:  1 * 2 * 3 * ... * x)
 
-fibo :: Integer -> Integer
-fibo n
-  | n == 0 = 0
-  | n == 1 = 1
-  | otherwise = fibo (n - 1) + fibo (n - 2)
+faNum :: Int -> Int
+faNum 1 = 1
+faNum n =  n * faNum ( n - 1)
 
-fibo' :: Integer -> Integer
-fibo' 0 = 0
-fibo' 1 = 1
-fibo' n = fibo' (n - 1) + fibo' (n - 2)
-
--- 4
-ggt :: Integer -> Integer -> Integer
-ggt x y
-  | x == y = x
-  | x > y = ggt (x - y) y
-  | otherwise = ggt x (y - x)
-
--- 5
-pot :: Integer -> Integer -> Integer
-pot _ 0 = 1
-pot x 1 = x
-pot x y = x * pot x (y - 1)
-
--- 6
-sum1n :: Integer -> Double
-sum1n 0 = 0
-sum1n 1 = 1
-sum1n n = 1.0 / fromIntegral n + sum1n (n - 1)
