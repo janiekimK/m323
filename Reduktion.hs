482695bcca05b0e0561f13933c46e633c492a418
Janie Kabashi <137904239+janiekimK@users.noreply.github.com>
2024-10-21 12:01:04 +0200

    Create Reduktion.hs
    Aufgabenstellung für 3 Aufgaben hinzugefügt

---
+++ Reduktion.hs
@@ -0,0 +1,11 @@
+--Aufgabe 1
+--Schreiben Sie eine Funktion, welche das Produkt der Elemente einer Liste berechnet. (Also aus z.B. [2,10,5] soll das Produkt 100 berechnet werden.)
+
+
+--Aufgabe 2
+--Schreiben Sie eine Funktion, welche das Produkt der geraden Elemente berechnet (also nicht jedes zweite, sondern das Produkt der geraden Zahlen in der Liste).
+
+
+--Aufgabe 3
+--Schreiben Sie eine Funktion mit foldl oder foldr, die eine Liste von positiven, ganzen Zahlen erhält und den grössten Wert in der Liste zurückgibt
+
