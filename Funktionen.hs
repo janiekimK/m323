c5869760ea26c0e188ba99a5dce9b51e3fee2a9e
Janie Kabashi <137904239+janiekimK@users.noreply.github.com>
2024-10-21 11:02:56 +0200

    Update Funktionen.hs
    Eigene Lösungen zu Funktionen eingefügt

--- Funktionen.hs
+++ Funktionen.hs
@@ -1,42 +1,38 @@
--- Beispiel:
+--Aufgabe 1
+--Schreiben Sie eine Funktion, die zwei ganze Zahlen als Parameter erhält und True zurückgibt, falls die beiden Werte gleich sind, sonst soll False zurückgegeben werden.
+func :: Int -> Int -> bool
+func x y 
+| x == y = True
+|otherwise = False
 
-mult :: Int -> Int -> Int
-mult x y = x * y
-
-
--- 1
-vergleich :: Int -> Int -> Bool
-vergleich x y = x == y
-
-
--- 2
-vergleich3 :: Int -> Int -> Int -> Bool
-vergleich3 x y z = x == y && y == z
+func :: Int -> Int -> bool
+func x y = x == y
 
 
--- 3
-bigger :: Double -> Double -> Double
-bigger x y
-    | x >= y = x
-    | otherwise = y
+--Aufgabe 2
+--Schreiben Sie eine Funktion, die drei ganze Zahlen als Parameter erhält und True zurückgibt, falls alle Werte gleich sind, 
+--sonst soll False zurückgegeben werden. Der AND-Operator heisst auch hier &&
 
 
--- 4
-quadriere :: Num a => a -> a
-quadriere x = x * x
 
 
--- 5
-beaufort :: Int -> [Char]
-beaufort x
-    | x <= 8 = "keine Gefahr"
-    | x == 9 = "Gefahr"
-    | x <= 12 = "erhebliche Gefahr"
-    | otherwise = "Wert ist zu gross"
+--Aufgabw 3
+--Schreiben Sie eine Funktion, die zwei Double-Zahlen als Parameter erhält. Die Funktion soll die grössere der beiden Zahlen als Ergebnis zurückgeben.
+
+biggerNum :: Double -> Double -> Double
+biggerNum x y
+| x > y = x
+| otherwise = y
 
 
--- 6
-rabattGeben :: Float -> Float -> Float -> Float
-rabattGeben preis limit rabatt
-    | preis > limit = preis * (1 - (rabatt / 100))
-    | otherwise = preis
+--Aufgabe 4
+--Schreiben Sie eine Funktion, die eine Zahl als Parameter erhält und den quadrierten Wert der Zahl als Ergebnis zurückliefert.
+
+quadNum :: Int -> Int
+quadNum x = x^2
+
+
+
+
+
+
